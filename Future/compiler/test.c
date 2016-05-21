381f944ece9eb675ad64969c2ed5920f822b7fc1
Bastien Penavayre <bastien@videolabs.io>
2016-05-21 14:26:19 +0200

    Custom gcc compiler finished

--- Future/compiler/test.c
+++ Future/compiler/test.c
@@ -1,24 +1,49 @@
 /*
-** test.c for  in /home/penava_b/perso/Obj/Obj4/Future/compiler
+** test.c for  in /home/penava_b
 ** 
 ** Made by penava_b
 ** Login   <penava_b@epitech.net>
 ** 
-** Started on  Wed May 11 02:12:22 2016 penava_b
-** Last update Wed May 11 03:11:05 2016 penava_b
+** Started on  Sat May 21 11:55:59 2016 penava_b
+** Last update Sat May 21 14:21:08 2016 penava_b
 */
 
-#include <stdio.h>
+#define __HASH_WORD__ #
+#define __NL__ /*
+*/
+#define __CREATE_MACRO__(symb, name, val, args...)      \
+    symb ifdef name __NL__                              \
+    symb undef name __NL__                              \
+    symb endif __NL__                                   \
+    symb define name(args) val __NL__
 
-#define new_pragma_string(data) ObjectC(#data)
-#define to_string2(data) #data
-#define to_string(data) to_string2(data)
-#define to_pragma(string) _Pragma(string)
+__CREATE_MACRO__(#, __POST_COMP_GLUE__, __POST_COMP_GLUE2__(x, y), x, y)
+__CREATE_MACRO__(#, __POST_COMP_GLUE2__, x ## y, x, y)
 
-to_pragma(to_string(new_pragma_string(yolo)))
+#define class(name)                                            \
+    __CREATE_MACRO__(__HASH_WORD__, CLASS, name)               \
+                                                               \
+    typedef struct name name; __NL__                           \
+                                                               \
+    struct name
+
+#define virtual                                                 \
+    }; __NL__                                                   \
+                                                                \
+       struct __POST_COMP_GLUE__(CLASS(),  _virtual)            \
+    {                                                           \
+        struct                                                  \
+
+#define method(name, ...) (*name)(CLASS() * const this, ##__VA_ARGS__)
+
+class(String)
+{
+    int i;
+    virtual {
+        void method(print);
+    };
+};
 
 int main()
 {
-    printf("Hello\n");
-    return 0;
 }
