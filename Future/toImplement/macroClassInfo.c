fb96a6bf54b67cf147fc1e8b95721be69b031a3c
Bastien Penavayre <bastienPenava@gmail.com>
2016-07-17 23:53:28 +0200

    Improve new file in future

--- Future/toImplement/macroClassInfo.c
+++ Future/toImplement/macroClassInfo.c
@@ -7,15 +7,12 @@
 #define GET_CLASS_INFO_NB_IMPLEMENTS(x, nb, ...)        \
     nb
 
-#define GET_CLASS_INFO(what, extends, nbInter, interfaces...)       \
-    GET_CLASS_INFO_ ## what(extends, nbInter, ##interfaces)
-
 #define CLASS_INFO_String(what)                 \
-    GET_CLASS_INFO(what, Object, 1, toString)
+    GET_CLASS_INFO_ ## what(Object, 2, toString, Serializable)
 
 #define CLASS_INFO(name, what)                  \
     CLASS_INFO_ ## name(what)
 
 CLASS_INFO(String, EXTENDS)
-CLASS_INFO(String, IMPLEMENTS)
 CLASS_INFO(String, NB_IMPLEMENTS)
+CLASS_INFO(String, IMPLEMENTS)
